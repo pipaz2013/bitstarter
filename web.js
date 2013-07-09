5ea7f68a955f2786b5b83922926de43a5ba401bc
Juan Pineros <pipaz2013@gmail.com>
2013-07-09 17:49:58 +0000

    prueba hola mundo 2

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   var fs = require("fs");
   var buffer = new Buffer(12);
 
-  response.send("Hola Mundo");
+  response.send("Hola Mundo 2");
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
