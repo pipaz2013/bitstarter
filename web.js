0124719b96a96fa1761691f07ca57855ab071df6
Juan Pineros <pipaz2013@gmail.com>
2013-07-09 14:23:54 +0000

    Changed in web.js to show index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fileName = ("./index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require("fs");
+  var buffer = new Buffer(12);
+  buffer.write(fileName);
+  response.send(fs.readFile(buffer.toString("utf-8")));
 });
 
 var port = process.env.PORT || 5000;
