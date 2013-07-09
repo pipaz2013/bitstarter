c3baeb58fb5ea90a583d121515aafc6da9e9d089
Juan Pineros <pipaz2013@gmail.com>
2013-07-09 15:15:29 +0000

    Updated web.js once again

--- web.js
+++ web.js
@@ -9,8 +9,6 @@ app.get('/', function(request, response) {
 
   response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
 });
-
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
