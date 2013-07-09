9f685534b61778b879f5777d5f3f51a82f432819
Juan Pineros <pipaz2013@gmail.com>
2013-07-09 19:16:09 +0000

    Esta si es solucion

--- web.js
+++ web.js
@@ -2,11 +2,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
- });
-
+  var fs = require('fs');
+  var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+  response.send(buffer.toString(buffer));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
